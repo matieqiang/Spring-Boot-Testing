b8adf791509573740fc45d9b179f92793014cede
matieqiang <tieqiang.ma@advancegroup.com>
2024-10-29 18:07:13 +0800

    add comments

--- employees/src/test/java/com/howtodoinjava/employees/EmployeesApplicationTests.java
+++ employees/src/test/java/com/howtodoinjava/employees/EmployeesApplicationTests.java
@@ -1,19 +1,34 @@
 package com.howtodoinjava.employees;
 
 import com.howtodoinjava.employees.controllers.EmployeeController;
+import com.howtodoinjava.employees.dao.EmployeeRepository;
+import com.howtodoinjava.employees.services.EmployeeService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * This '@SpringBootTest' annotation is used to load complete application context for end to end integration testing
+ * The @SpringBootTest annotation can be used when we need to bootstrap the entire container.
+ * The annotation works by creating the ApplicationContext that will be utilized in our tests.
+ */
 @SpringBootTest
 public class EmployeesApplicationTests {
 
   @Autowired
   EmployeeController employeeController;
 
+  @Autowired
+  EmployeeRepository employeeRepository;
+
+  @Autowired
+  EmployeeService employeeService;
+
   @Test
   public void contextLoads() {
     Assertions.assertThat(employeeController).isNotNull();
+    Assertions.assertThat(employeeRepository).isNotNull();
+    Assertions.assertThat(employeeService).isNotNull();
   }
 }
--- employees/src/test/java/com/howtodoinjava/employees/controllers/StandaloneControllerTests.java
+++ employees/src/test/java/com/howtodoinjava/employees/controllers/StandaloneControllerTests.java
@@ -6,8 +6,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.howtodoinjava.employees.model.Employee;
 import com.howtodoinjava.employees.services.EmployeeService;
+
 import java.util.Arrays;
 import java.util.List;
+
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -16,26 +18,40 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+/**
+ * '@WebMvcTest'
+ * <p>
+ * 1. '@WebMvcTest' Provides an easy way to configure and execute unit tests for Spring MVC.
+ * It focuses on testing the behavior of components such as @ControllerController, @RestController, and @ControllerAdvice without launching the full application context.
+ * <p>
+ * 2. Using the @WebMvcTest annotation allows you to specify the controller class to be tested,
+ * while automatically configuring the required beans.
+ * This increases the efficiency of testing and reduces test dependencies.
+ * <p>
+ * 3. This allows you to simulate and validate requests using MockMvc objects during testing.
+ *
+ * 'MockMvc class,'
+ */
 @WebMvcTest(EmployeeController.class)
 public class StandaloneControllerTests {
 
-  @MockBean
-  EmployeeService employeeService;
+    @MockBean
+    EmployeeService employeeService;
 
-  @Autowired
-  MockMvc mockMvc;
+    @Autowired
+    MockMvc mockMvc;
 
-  @Test
-  public void testfindAll() throws Exception {
-    Employee employee = new Employee("Lokesh", "Gupta");
-    List<Employee> employees = Arrays.asList(employee);
+    @Test
+    public void testfindAll() throws Exception {
+        Employee employee = new Employee("Lokesh", "Gupta");
+        List<Employee> employees = Arrays.asList(employee);
 
-    Mockito.when(employeeService.findAll()).thenReturn(employees);
+        Mockito.when(employeeService.findAll()).thenReturn(employees);
 
-    mockMvc.perform(get("/employee"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$", Matchers.hasSize(1)))
-        .andExpect(jsonPath("$[0].firstName", Matchers.is("Lokesh")));
-  }
+        mockMvc.perform(get("/employee"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.hasSize(1)))
+                .andExpect(jsonPath("$[0].firstName", Matchers.is("Lokesh")));
+    }
 
 }
